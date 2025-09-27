363edee7a351cfba54b7501ec187d3c0fc911597
gala <galymbartai@gmail.com>
2025-09-27 20:06:31 +0500

    feat(shapes): add abstract Shape class

--- src/assignment4/shapes/Shape.java
+++ src/assignment4/shapes/Shape.java
@@ -1,4 +1,12 @@
 package assignment4.shapes;
 
-public class Shape {
+import assignment4.color.interfaces.IColor;
+
+public abstract class Shape {
+    protected IColor color;
+    public Shape(IColor color) {
+        this.color = color;
+    }
+
+    public abstract void draw();
 }
