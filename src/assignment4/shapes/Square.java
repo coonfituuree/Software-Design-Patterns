1935f531d2b5a962d9538cf6fc399ad4af2cd426
gala <galymbartai@gmail.com>
2025-09-27 20:06:42 +0500

    feat(shapes): implement Square shape

--- src/assignment4/shapes/Square.java
+++ src/assignment4/shapes/Square.java
@@ -1,4 +1,14 @@
 package assignment4.shapes;
 
-public class Square {
+import assignment4.color.interfaces.IColor;
+
+public class Square extends Shape {
+    public Square(IColor color) {
+        super(color);
+    }
+
+    @Override
+    public void draw() {
+        System.out.println("Drawing Square in " + color.fill() + " color");
+    }
 }
