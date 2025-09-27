48dcd5a478fdb3eb31683a66d947745a4d9d91cb
gala <galymbartai@gmail.com>
2025-09-27 20:06:38 +0500

    feat(shapes): implement Circle shape

--- src/assignment4/shapes/Circle.java
+++ src/assignment4/shapes/Circle.java
@@ -1,4 +1,13 @@
 package assignment4.shapes;
 
-public class Circle {
+import assignment4.color.interfaces.IColor;
+
+public class Circle extends Shape {
+    public Circle(IColor color) {
+        super(color);
+    }
+    @Override
+    public void draw() {
+        System.out.println("Drawing Circle in " + color.fill() + " color");
+    }
 }
