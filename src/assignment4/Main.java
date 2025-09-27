dfa72e040b56b766fee3d56e2e8dec0b93c5dc5c
gala <galymbartai@gmail.com>
2025-09-27 20:07:35 +0500

    chore(demo): add Main with test run

--- src/assignment4/Main.java
+++ src/assignment4/Main.java
@@ -1,4 +1,17 @@
 package assignment4;
 
+import assignment4.color.Blue;
+import assignment4.color.Red;
+import assignment4.shapes.Circle;
+import assignment4.shapes.Shape;
+import assignment4.shapes.Square;
+
 public class Main {
+    public static void main(String[] args) {
+        Shape redCircle = new Circle(new Red());
+        Shape blueSquare = new Square(new Blue());
+
+        redCircle.draw();
+        blueSquare.draw();
+    }
 }
